28fe24f4608712c7d85569053ba4b70a5acf1c8e
tSolodkaya <solodkayagit@gmail.com>
2023-09-27 15:52:53 +0200

    feat: add carusel library for explore, testimonial, shots

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+$(".category-menu").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: "[data-prev-explore]",
+  nextArrow: "[data-next-explore]",
+});
+
+$(".testimonials-list").slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll1: 3,
+  centerMode: true,
+
+  prevArrow: "[data-prev-testimonials]",
+  nextArrow: "[data-next-testimonials]",
+});
+
+$(".shots-list").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: "[data-prev-shots]",
+  nextArrow: "[data-next-shots]",
+});
